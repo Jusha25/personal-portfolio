63a1c616b95593457f7f47a4dc120ffbab25d2c3
shaili <shaili.sharma0@gmail.com>
2022-06-21 22:58:23 +0530

    fix linters

--- menu.js
+++ menu.js
@@ -1,14 +1,14 @@
-const hamburger = document.querySelector(".hamburger");
-const rightnav = document.querySelector(".right-nav");
+const hamburger = document.querySelector('.hamburger');
+const rightnav = document.querySelector('.right-nav');
 
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  rightnav.classList.toggle("active");
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  rightnav.classList.toggle('active');
 });
 
-document.querySelectorAll(".nav-link").foreach((n) =>
-  n.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    rightnav.classList.remove("active");
+document.querySelectorAll('.nav-link').foreach((n) =>
+  n.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    rightnav.classList.remove('active');
   })
 );
