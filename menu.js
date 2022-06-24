dc11d42e236c264720aa87bfa7e6b8522974adb3
shaili <shaili.sharma0@gmail.com>
2022-06-24 15:32:49 +0530

    Fix Linters

--- menu.js
+++ menu.js
@@ -1,34 +1,34 @@
-const toggleMenu = document.querySelector(".toggle-menu");
-const closeBtn = document.querySelector("#close-btn");
-const menuItems = document.querySelectorAll(".menu-item");
+const toggleMenu = document.querySelector('.toggle-menu');
+const closeBtn = document.querySelector('#close-btn');
+const menuItems = document.querySelectorAll('.menu-item');
 // for-top-nav
 function openNav() {
-  document.querySelector(".mobile-menu").classList.add("active");
-  toggleMenu.classList.add("invisible");
-  document.body.style.overflow = "hidden";
+  document.querySelector('.mobile-menu').classList.add('active');
+  toggleMenu.classList.add('invisible');
+  document.body.style.overflow = 'hidden';
 }
 
-toggleMenu.addEventListener("click", openNav);
+toggleMenu.addEventListener('click', openNav);
 
 function closeNav() {
-  document.querySelector(".mobile-menu").classList.remove("active");
-  toggleMenu.classList.remove("invisible");
-  document.body.style.overflow = "auto";
+  document.querySelector('.mobile-menu').classList.remove('active');
+  toggleMenu.classList.remove('invisible');
+  document.body.style.overflow = 'auto';
 }
 
-closeBtn.addEventListener("click", closeNav);
+closeBtn.addEventListener('click', closeNav);
 
 menuItems.forEach((item) => {
-  item.addEventListener("click", closeNav);
+  item.addEventListener('click', closeNav);
 });
 
 // for scrolling effect //
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-  anchor.addEventListener("click", function (e) {
+  anchor.addEventListener('click', function (e) {
     e.preventDefault();
 
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth",
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth',
     });
   });
 });
