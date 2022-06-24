99e34e76f91c5660f849b66adf5b09951da81b8e
shaili <shaili.sharma0@gmail.com>
2022-06-24 17:56:51 +0530

    run eslinters

--- popup.js
+++ popup.js
@@ -1,54 +1,56 @@
 const projects = [
   {
-    name: "Tonic",
-    devInfo: "CANOPY",
+    name: 'Tonic',
+    devInfo: 'CANOPY',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent, Lorem Ipsum is simply dummy text.",
-    languages: ["html", "css", "javascript"],
-    image: "./images/snapshoot-portfolio.png",
-    liveVersion: "See Live",
-    sourceLink: "See Source",
+    languages: ['html', 'css', 'javascript'],
+    image: './images/snapshoot-portfolio.png',
+    liveVersion: 'See Live',
+    sourceLink: 'See Source',
   },
 
   {
-    name: "Multi-Post Stories",
-    devInfo: "CANOPY",
+    name: 'Multi-Post Stories',
+    devInfo: 'CANOPY',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent, Lorem Ipsum is simply dummy text.",
-    languages: ["html", "css", "javascript", "React"],
-    image: "./images/SnapshootPortfolio2.png",
-    liveVersion: "See Live",
-    sourceLink: "See Source",
+    languages: ['html', 'css', 'javascript', 'React'],
+    image: './images/SnapshootPortfolio2.png',
+    liveVersion: 'See Live',
+    sourceLink: 'See Source',
   },
 
   {
-    name: "Tonic",
-    devInfo: "CANOPY",
+    name: 'Tonic',
+    devInfo: 'CANOPY',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent, Lorem Ipsum is simply dummy text.",
-    languages: ["html", "css", "javascript"],
-    image: "./images/SnapshootPortfolio3.png",
-    liveVersion: "See Live",
-    sourceLink: "See Source",
+    languages: ['html', 'css', 'javascript'],
+    image: './images/SnapshootPortfolio3.png',
+    liveVersion: 'See Live',
+    sourceLink: 'See Source',
   },
 
   {
-    name: "Multi-Post Stories",
-    devInfo: "CANOPY",
+    name: 'Multi-Post Stories',
+    devInfo: 'CANOPY',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent, Lorem Ipsum is simply dummy text.",
-    languages: ["html", "css", "javascript"],
-    image: "./images/SnapshootPortfolio4.png",
-    liveVersion: "See Live",
-    sourceLink: "See Source",
+    languages: ['html', 'css', 'javascript'],
+    image: './images/SnapshootPortfolio4.png',
+    liveVersion: 'See Live',
+    sourceLink: 'See Source',
   },
 ];
 
-const projectSection = document.getElementById("project-section");
+const projectSection = document.getElementById('project-section');
 
 // for adding projects to the display
 function addProjects(n) {
-  const { name, devInfo, description, languages, image } = projects[n];
+  const {
+    name, devInfo, description, languages, image,
+  } = projects[n];
 
   projectSection.innerHTML += `
 <div class="project">
@@ -87,12 +89,12 @@ for (let i = 0; i < projects.length; i += 1) {
   addProjects(i);
 }
 
-const seeProjectBtn1 = document.querySelector(".project0-link"); // get-project btns
-const seeProjectBtn2 = document.querySelector(".project1-link"); // get-project btns
-const seeProjectBtn3 = document.querySelector(".project2-link"); // get-project btns
-const seeProjectBtn4 = document.querySelector(".project3-link"); // get-project btns
-const overlay = document.querySelector("#overlay"); // get overlay
-const container = document.querySelector(".pop-up-window"); // get the popup-window
+const seeProjectBtn1 = document.querySelector('.project0-link'); // get-project btns
+const seeProjectBtn2 = document.querySelector('.project1-link'); // get-project btns
+const seeProjectBtn3 = document.querySelector('.project2-link'); // get-project btns
+const seeProjectBtn4 = document.querySelector('.project3-link'); // get-project btns
+const overlay = document.querySelector('#overlay'); // get overlay
+const container = document.querySelector('.pop-up-window'); // get the popup-window
 
 // open-popup function
 function openPopup(index) {
@@ -153,68 +155,68 @@ function openPopup(index) {
 </div>`;
 
   // then add class of active to them
-  container.classList.add("active");
-  overlay.classList.add("active");
-  document.body.style.overflow = "hidden";
+  container.classList.add('active');
+  overlay.classList.add('active');
+  document.body.style.overflow = 'hidden';
 
   // close-popup function
-  const popupCloseBtn = document.querySelector(".popup-close-btn"); // get the close btn
-  popupCloseBtn.addEventListener("click", () => {
+  const popupCloseBtn = document.querySelector('.popup-close-btn'); // get the close btn
+  popupCloseBtn.addEventListener('click', () => {
     // remove active from class
-    container.classList.remove("active");
-    overlay.classList.remove("active");
-    document.body.style.overflow = "auto"; // for turning the background scrolling effect off
+    container.classList.remove('active');
+    overlay.classList.remove('active');
+    document.body.style.overflow = 'auto'; // for turning the background scrolling effect off
   });
   // remove class active if user click outside the popup-window
-  overlay.addEventListener("click", () => {
-    container.classList.remove("active");
-    overlay.classList.remove("active");
-    document.body.style.overflow = "auto"; // for turning the background scrolling effect off
+  overlay.addEventListener('click', () => {
+    container.classList.remove('active');
+    overlay.classList.remove('active');
+    document.body.style.overflow = 'auto'; // for turning the background scrolling effect off
   });
 }
 
 // calling open function if user clicks on see project btn
-seeProjectBtn1.addEventListener("click", () => {
+seeProjectBtn1.addEventListener('click', () => {
   openPopup(0);
 });
 
-seeProjectBtn2.addEventListener("click", () => {
+seeProjectBtn2.addEventListener('click', () => {
   openPopup(1);
 });
 
-seeProjectBtn3.addEventListener("click", () => {
+seeProjectBtn3.addEventListener('click', () => {
   openPopup(2);
 });
 
-seeProjectBtn4.addEventListener("click", () => {
+seeProjectBtn4.addEventListener('click', () => {
   openPopup(3);
 });
 
 // form validation
 
-const form = document.querySelector("form"); // get the form
-const username = document.getElementById("name"); // get the name
-const email = document.getElementById("email"); // get the email
-const message = document.getElementById("message"); // get the message
+const form = document.querySelector('form'); // get the form
+const username = document.getElementById('name'); // get the name
+const email = document.getElementById('email'); // get the email
+const message = document.getElementById('message'); // get the message
 
 // for setting the error message
 const setError = (element, message) => {
   const inputControl = element.parentElement;
-  const errorDisplay = inputControl.querySelector(".errorMsg");
+  const errorDisplay = inputControl.querySelector('.errorMsg');
 
   errorDisplay.innerText = message;
-  inputControl.classList.add("error");
-  inputControl.classList.remove("success");
+  inputControl.classList.add('error');
+  inputControl.classList.remove('success');
 };
 
 // for setting the the success message
 const setSuccess = (element) => {
   const inputControl = element.parentElement;
-  const errorDisplay = inputControl.querySelector(".errorMsg");
+  const errorDisplay = inputControl.querySelector('.errorMsg');
 
-  errorDisplay.innerText = "";
-  inputControl.classList.add("success");
-  inputControl.classList.remove("error");
+  errorDisplay.innerText = '';
+  inputControl.classList.add('success');
+  inputControl.classList.remove('error');
 };
 
 let submitPt = 0; // for calcuating the result should be 3
@@ -225,36 +227,36 @@ const validateInputs = () => {
   const messageValue = message.value.trim();
 
   // for name validation
-  if (usernameValue === "") {
-    setError(username, "Username is required");
+  if (usernameValue === '') {
+    setError(username, 'Username is required');
     return;
   }
-  if (usernameValue.split("").length > 30) {
-    setError(username, "Username should not exceed 30 characters");
+  if (usernameValue.split('').length > 30) {
+    setError(username, 'Username should not exceed 30 characters');
     return;
   }
   setSuccess(username);
   submitPt += 1;
 
   // for email validation
-  if (emailValue === "") {
-    setError(email, "Email is required");
+  if (emailValue === '') {
+    setError(email, 'Email is required');
     return;
   }
   if (emailValue !== emailValue.toLowerCase()) {
-    setError(email, "Email should be in lowercase");
+    setError(email, 'Email should be in lowercase');
     return;
   }
   setSuccess(email);
   submitPt += 1;
 
   // for message validation
-  if (messageValue === "") {
-    setError(message, "Please add a message");
-  } else if (messageValue.split("").length > 500) {
+  if (messageValue === '') {
+    setError(message, 'Please add a message');
+  } else if (messageValue.split('').length > 500) {
     setError(
       message,
-      "Message should be short and do not exceed 500 characters"
+      'Message should be short and do not exceed 500 characters',
     );
   } else {
     setSuccess(message);
@@ -263,7 +265,7 @@ const validateInputs = () => {
 };
 
 // the validation when the form is submitted
-form.addEventListener("submit", (e) => {
+form.addEventListener('submit', (e) => {
   e.preventDefault(); // prevent sumitting
 
   validateInputs(); // run checks
