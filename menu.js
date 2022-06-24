7a0bd85634696e2e91a0d422d0eff0b7670233e6
shaili <shaili.sharma0@gmail.com>
2022-06-24 15:30:30 +0530

    fix JS Linters

--- menu.js
+++ menu.js
@@ -32,26 +32,3 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
     });
   });
 });
-
-// for highlighting nav-menu//
-
-const sections = document.querySelectorAll(".page-section");
-const navLi = document.querySelectorAll("nav ul li");
-
-window.addEventListener("scroll", () => {
-  let currentSection = "";
-
-  sections.forEach((section) => {
-    const sectionTop = section.offsetTop;
-    const sectionHeight = section.clientHeight;
-    if (pageYOffset >= sectionTop - sectionHeight / 3.5) {
-      currentSection = section.getAttribute("id");
-    }
-  });
-  navLi.forEach((list) => {
-    list.classList.remove("active");
-    if (list.classList.contains(currentSection)) {
-      list.classList.add("active");
-    }
-  });
-});
